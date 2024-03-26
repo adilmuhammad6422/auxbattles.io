a4ebc04ba9e0c1bde60f6dbaee0aab06efba342a
eners49 <32853967+eners49@users.noreply.github.com>
2024-03-26 16:09:03 -0400

    Update home.jsx

--- auxbattles/src/home.jsx
+++ auxbattles/src/home.jsx
@@ -1,6 +1,8 @@
 function home() {
     return <div>
-      <h1 style={{fontFamily: "Courier New"}}>A</h1><h3 style={{fontFamily: "Courier New"}}>UXBATTLES</h3><h1 style={{fontFamily: "Courier New"}}>.IO</h1>
+      <h1 style={{fontFamily: "Courier New"}}>A</h1>
+      <h3 style={{fontFamily: "Courier New"}}>UXBATTLES</h3>
+      <h1 style={{fontFamily: "Courier New"}}>.IO</h1>
     </div>;
   }
   
