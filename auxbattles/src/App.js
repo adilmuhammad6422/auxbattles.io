2346db0ce4d227d58617902a7bfdbdecab36fcb3
JOSEPH-ANDEL <155003094+JOSEPH-ANDEL@users.noreply.github.com>
2024-03-28 17:50:51 -0400

    Fixed Load Times
    Fixed Load Times

--- auxbattles/src/App.js
+++ auxbattles/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/message');
+      const response = await axios.get('http://localhost:8080/message');
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
