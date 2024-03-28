58aa6396344baa4416315f75b02a19622903f3b9
JOSEPH-ANDEL <155003094+JOSEPH-ANDEL@users.noreply.github.com>
2024-03-28 16:15:46 -0400

    Setup backend
    I think I made the backend: To add new dependencies: npm install axios cors in auxbattles folder and npm install express body-parser in server folder

--- auxbattles/src/App.js
+++ auxbattles/src/App.js
@@ -1,28 +1,47 @@
-// App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route/*, Link*/ } from 'react-router-dom';
 import HomePage from './home';
 import GamePage from './game';
 import VotingPage from './voting';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/message');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
   return (
-    <Router>
-      <div>
-        {/*<nav>
+    <div>
+      <Router>
+        <div>
+          {/*<nav>
           <Link to="/">Home</Link> |{" "}
           <Link to="/game">Game</Link> |{" "}
           <Link to="/vote">Vote</Link>
   </nav>*/}
 
-        {/* Routes setup */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/vote" element={<VotingPage />} />
-        </Routes>
-      </div>
-    </Router>
+          {/* Routes setup */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/game" element={<GamePage />} />
+            <Route path="/vote" element={<VotingPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
