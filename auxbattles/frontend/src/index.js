f0b4fbefb8c81991c4cb0068c5c87919e82735f1
JOSEPH-ANDEL <155003094+JOSEPH-ANDEL@users.noreply.github.com>
2024-04-15 22:28:22 -0400

    Added option to run on localhost again
    Now automatically chooses to run on localhost if it is necessary.

--- auxbattles/frontend/src/index.js
+++ auxbattles/frontend/src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://backend-service-dot-red-welder-418620.ue.r.appspot.com/';
+
+// Check if running in localhost
+const isLocalhost = window.location.hostname === 'localhost';
+const baseURL = isLocalhost
+  ? 'http://localhost:8080' // Replace YOUR_PORT_NUMBER_HERE with the port your backend is running on
+  : 'https://backend-service-dot-red-welder-418620.ue.r.appspot.com/';
+
+
+axios.defaults.baseURL = baseURL;
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
